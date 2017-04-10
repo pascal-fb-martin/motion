136709a35d495511b863a1da9aeedcbfffec8872
Daniel Richard G <skunk@iSKUNK.ORG>
2017-04-10 17:12:02 -0400

    Don't put RTSP URL passwords into the log
    When logging, use cnt->conf.netcam_url instead of rtsp->path, as the
    latter may contain a security-sensitive "user:password@" string. We add
    a new rtsp->netcam_url field that is a read-only pointer to the former.
    
    Also, don't try to LOG("%s", NULL).

--- netcam_rtsp.h
+++ netcam_rtsp.h
@@ -23,6 +23,7 @@ struct rtsp_context {
     char*                 path;
     char*                 user;
     char*                 pass;
+    const char*           netcam_url;
     int                   interrupted;
     int                   active;
     enum RTSP_STATUS      status;
